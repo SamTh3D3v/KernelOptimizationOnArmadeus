d9ea48136ca1e5ca1c463a22c0bafe28613d34a5
Oussama Elhamer <oussama.elhamer@outlook.com>
2016-12-14 05:31:18 +0100

    add test files to the lynx folder

---
+++ mm/lynx/Test_files/mmaptest.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/io.h>
+#include <sys/mman.h>
+
+int main(int argc, char const *argv[])
+{
+    unsigned char *f;
+    int size;
+    int i;
+    struct stat s;
+    const char * file_name = argv[1];
+    int fd = open (argv[1], O_RDONLY);
+
+    /* Get the size of the file. */
+    int status = fstat (fd, & s);
+    size = s.st_size;
+
+    f = (char *) mmap (0, size, PROT_READ, MAP_PRIVATE, fd, 0);
+    for ( i = 0; i < size; i++) {
+        char c;
+
+        c = f[i];
+        //putchar(c);
+    }
+    printf("\n done reading the file \n");
+    return 0;
+}
